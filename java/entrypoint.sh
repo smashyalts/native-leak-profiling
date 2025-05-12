2723bae763675177d1ccc8c28cf4734a03b19693
Skullian <133133667+Skullians@users.noreply.github.com>
2025-05-12 18:56:50 +0100

    Initial work on thread dump analysis

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -77,5 +77,42 @@ mkdir -p dumps
     done
 ) &
 
+(
+    sleep 60
+    PID=$(pgrep java)
+    INPUT_FILE="dumps/jvm.log"
+    OUTPUT_FILE="dumps/matches.log"
+    KEYWORD=$(echo "$PARSED" | sed -n 's/.*-Dkeyword=\([^ ]*\).*/\1/p')
+    ENABLED=$(echo "$PARSED" | sed -n 's/.*-Danalyse=\([^ ]*\).*/\1/p')
+    
+    if [ "${ENABLED}" = "true" ]; then
+        if [ -z "$PID" ]; then
+            printf "No Java process found\n"
+        else
+            printf "Java process PID: $PID. Starting thread dumps.\n"
+            while true; do
+                kill -3 ${PID}
+                
+                tail -f "$INPUT_FILE" | while read -r line; do
+                    if [[ "$line" =~ ^"Full thread dump" ]]; then
+                        buffer=""
+                        while read -r line; do
+                            buffer+="$line"$'\n'
+                            if [[ "$line" =~ ^"VM Thread" ]]; then
+                                if echo "$buffer" | grep -q "$KEYWORD"; then
+                                    echo "Found matching trace at $(date)" >> "$OUTPUT_FILE"
+                                    echo "$buffer" >> "$OUTPUT_FILE"
+                                    echo "---" >> "$OUTPUT_FILE"
+                                fi
+                                break
+                            fi
+                        done
+                    fi
+                done
+            done
+        fi
+    fi
+) &
+
 # shellcheck disable=SC2086
 exec env ${PARSED}
